8dc2c72a47633899cee6965aeb1753ea3e44b4eb
Serena McLaughlin <serenajmclaughlin@gmail.com>
2018-11-30 17:07:29 -0600

    adding third commit

---
+++ week-1/McLaughlin-Assignment-1-4.js
@@ -0,0 +1,20 @@
+var firstName = "Serena";
+console.log(firstName);
+
+var lastName = "McLaughlin";
+console.log(lastName);
+
+var age = "29";
+console.log(age);
+
+var street = "1487 N. S St."
+console.log(street);
+
+var city = "Omaha";
+console.log(city);
+
+var state = "Nebraska";
+console.log(state);
+
+var zipCode = "68064";
+console.log(zipCode);
