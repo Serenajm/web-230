fd0f40f6ee3f1a029cc7e86b8a49ee0dc73ad795
Serena McLaughlin <serenajmclaughlin@gmail.com>
2018-12-01 15:33:56 -0600

    Adding forth commit

---
+++ week-1/McLaughlin-Assignment-1-5.js
@@ -0,0 +1,80 @@
+var myFirstName = "Serena";
+var myLastName = "McLaughlin";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n");
+
+
+var firstName1 = "Alex";
+var lastName1 = "Timms";
+var address1 = "570 7th St.";
+var payRate1 = 10.50;
+var hireDate1 = new Date(2014, 3, 7);
+
+var person1 = "\nFirst Name: " + firstName1 + "\nLast Name: " 
+    + lastName1 + "\nAddress: " + address1 + "\nPayrate: $" 
+    + payRate1.toFixed(2) + "\nHire Date: " + hireDate1.toLocaleDateString();
+
+console.log(person1);
+console.log("\n");
+
+
+var firstName2 = "Carren";
+var lastName2 = "Sebastian";
+var address2 = "2089 W. 14th Rd.";
+var payRate2 = 15.00;
+var hireDate2 = new Date(2016, 7, 29);
+
+var person2 = "\nFirst Name: " + firstName2 + "\nLast Name: " 
+    + lastName2 + "\nAddress: " + address2 + "\nPayrate: $" 
+    + payRate2.toFixed(2) + "\nHire Date: " + hireDate2.toLocaleDateString();
+
+console.log(person2);
+console.log("\n");
+
+
+var firstName3 = "Pippin";
+var lastName3 = "Tabasco";
+var address3 = "6087 Baxter Ave.";
+var payRate3 = 13.75;
+var hireDate3 = new Date(2017, 10, 17);
+
+var person3 = "\nFirst Name: " + firstName3 + "\nLast Name: " 
+    + lastName3 + "\nAddress: " + address3 + "\nPayrate: $" 
+    + payRate3.toFixed(2) + "\nHire Date: " + hireDate3.toLocaleDateString();
+
+console.log(person3);
+console.log("\n");
+
+
+var firstName4 = "Teddy";
+var lastName4 = "Sprinkles";
+var address4 = "572 Happy Hollow Lane";
+var payRate4 = 20.00;
+var hireDate4 = new Date(2005, 6, 19);
+
+var person4 = "\nFirst Name: " + firstName4 + "\nLast Name: " 
+    + lastName4 + "\nAddress: " + address4 + "\nPayrate: $" 
+    + payRate4.toFixed(2) + "\nHire Date: " + hireDate4.toLocaleDateString();
+
+console.log(person4);
+console.log("\n");
+
+
+var firstName5 = "Frisbee";
+var lastName5 = "Jones";
+var address5 = "37100 Abraham Rd.";
+var payRate5 = 19.25;
+var hireDate5 = new Date(2008, 5, 10);
+
+var person5 = "\nFirst Name: " + firstName5 + "\nLast Name: " 
+    + lastName5 + "\nAddress: " + address5 + "\nPayrate: $" 
+    + payRate5.toFixed(2) + "\nHire Date: " + hireDate5.toLocaleDateString();
+
+console.log(person5);
+console.log("\n");
